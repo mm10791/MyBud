9e4fc62b5d40ae4b7e6c4d2a0ac2036621a5a824
Max Matthews <122639096+mm10791@users.noreply.github.com>
2025-03-10 16:40:04 -0400

    Add files via upload
    Changed project format to 15.3 for compatibility with VMs

---
+++ MyBud/MyBud/MyBud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBud
+//
+//  Created by Max on 2025-03-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
